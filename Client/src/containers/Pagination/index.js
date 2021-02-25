14082f9ce5897837e7868232637e0f9dceb010a2
Henry Cortez <Henry200amaya@gmail.com>
2021-02-25 00:10:01 -0600

    Add: dinamic pagination

---
+++ Client/src/containers/Pagination/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// CSS FILES  
+import './Pagination.css'
+
+// ICONS
+import { faArrowRight, faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Pagination({ page, setPage, minPage, maxPage }){
+
+  console.log({ minPage, maxPage })
+  return (
+    <div className="pagination">
+      <button className="pagination-button" onClick={() =>  
+          setPage(currentPage => currentPage == minPage ? currentPage : currentPage - 1)}>
+        <FontAwesomeIcon icon={faArrowLeft} className='pagination-change' />
+      </button>
+      <button className="pagination-button pagination-change">
+        {page}
+      </button>
+      <button className="pagination-button" >
+        <FontAwesomeIcon icon={faArrowRight} className='pagination-change' onClick={() => 
+            setPage(currentPage => currentPage == maxPage ? currentPage : currentPage + 1)}/>
+      </button>
+    </div>
+  )
+}
